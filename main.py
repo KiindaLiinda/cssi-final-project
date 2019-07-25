fd4ce655afd69186e379355d3f361a87e97e8d89
Linda J. Garcia <thenegativejuan@gmail.com>
2019-07-24 17:53:31 -0700

    Created form to create a new event and edited main.py for the form

--- main.py
+++ main.py
@@ -11,15 +11,30 @@ jinja_current_directory = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-#Google Login API
-
-#Google Calendar API
+#Homepage Handler
+class HomepageHandler(webapp2.RequestHandler):
+    def get(self):
+        start_template = jinja_current_dir.get_template("templates/welcome.html")
+        self.response.write(start_template.render())
 
 #Embedding Google Calendar
-def add_event(self):
-    event_name = self.request.get('event-name')
-    event_time_start = self.request.get('event-time-start')
-    event_time_end = self.request.get('event-time-end')
-    event_cat = self.request.get('event-cat')
+class CalendarHandler(webapp2.RequestHandler):
+    def get(self):
+        start_string = self.request.get('starttime')
+        start_date = datetime.strptime(start_string, "%Y-%m-%dT%H:%M")
+        start_utc = start_date + timedelta(hours=7)
+        end_utc = start_utc + timedelta(hours=1)
 
-def remove_event(self):
+        calendar_url = "http://www.google.com/calendar/event?action=TEMPLATE&text=%s&dates=%s/%s"
+        calendar_start = start_utc.strftime("%Y%m%dT%H%M00Z")
+        calendar_end = end_utc.strftime("%Y%m%dT%H%M00Z")
+
+        calendar_link = calendar_url % ("TestEvent",calendar_start, calendar_end)
+        calendar_html = "<html><body><a target='_blank' href='%s'>Test Event Link</a></body></html>"
+        self.response.write(calendar_html % calendar_link)
+
+app = webapp2.WSGIApplication([
+    ('/', FoodHandler),
+    ('/showfavs', ShowFoodHandler),
+    ('/calendar', CalendarHandler),
+], debug=True)
